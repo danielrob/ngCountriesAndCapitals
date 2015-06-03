5428396d35402ceb598eec007fad6b2afef18750
Daniel Robinson <djr.self@gmail.com>
2015-06-03 12:07:10 -0400

    add loading animations

--- app/js/cac-app.js
+++ app/js/cac-app.js
@@ -19,25 +19,32 @@ app.config(['$routeProvider', function($routeProvider){
   })
 }]);
 
-app.controller('countryCtrl', ['$http', '$scope', 'country', function($http, $scope, country) {
+app.controller('countryCtrl', ['$http', '$scope', '$q', 'country', function($http, $scope, $q, country) {
+  $scope.loading = true;
   $http.get('http://api.geonames.org/countryInfoJSON?country='+country+'&username=danielrob', {cache: true}).success(function(data) {
     console.log(data.geonames[0]);
     $scope.ctry = data.geonames[0];
-    $http.get('http://api.geonames.org/neighboursJSON?geonameId='+$scope.ctry.geonameId+'&username=danielrob', {cache: true}).success(function(data) {
+    var p1 = $http.get('http://api.geonames.org/neighboursJSON?geonameId='+$scope.ctry.geonameId+'&username=danielrob', {cache: true}).success(function(data) {
       console.log(data)
       $scope.ctry.neighbours = data.geonames;
     });
-    $http.get('http://api.geonames.org/searchJSON?q='+$scope.ctry.capital+'&username=danielrob', {cache: true}).success(function(data) {
+    var p2 = $http.get('http://api.geonames.org/searchJSON?q='+$scope.ctry.capital+'&username=danielrob', {cache: true}).success(function(data) {
       console.log(data.geonames[0])
       $scope.ctry.capitalresolved = data.geonames[0];
+    });
+    $q.all([p1,p2]).then(function(){
+      console.log('here')
+      $scope.loading = false;
     });
   });
 }]);
 
 app.controller('default', ['$scope', '$http', '$location', function($scope, $http, $location){ 
+  $scope.loading = true;
   $http.get('http://api.geonames.org/countryInfoJSON?username=danielrob', { cache: true}).success(function(data) {
      console.log(data.geonames);
      $scope.countries = data.geonames;
+     $scope.loading = false;
   });
   $scope.showCountry = function(country) {
     $location.path('countries/' + country.countryCode);
